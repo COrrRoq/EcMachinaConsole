4c23db93c173f210a558c1c5c21e91e4380b19f4
COrrRoq <catherine.orr@roq.co.uk>
2023-02-09 12:42:22 +0000

    Tidied

--- index.js
+++ index.js
@@ -6,20 +6,25 @@ const names = [];
 let path = []
 
 
-while(entered != 'QUIT'){
-   command = prompt ('What would you like to do? ');
-   entered = command.toUpperCase();
+while (entered != 'QUIT') {
+    command = prompt('What would you like to do? ');
+    entered = command.toUpperCase();
 
-   if(entered == 'SCREAM'){
-    console.log('We scream. Now we got that out of your system...');
+    if (entered == 'SCREAM') {
+        console.log('We scream. Now we got that out of your system...');
     }
 
-    if(entered == 'START'){
-        let name = prompt ('Awesome! First, what is your name? ')
-        names.push (name)
-        console.log("Hi ",(names),'!')
-        let path = prompt ('Which door will you take? Left or right?')
-    }
-    
-    
-};
+    if (entered == 'START') {
+        let name = prompt('Awesome! First, what is your name? ')
+        names.push(name)
+        console.log("Hi ", (names), '!')
+        let path = prompt('Which door will you take? Left or right?')
+        }
+    };
+
+
+
+
+
+
+
